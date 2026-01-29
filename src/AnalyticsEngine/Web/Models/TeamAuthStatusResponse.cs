f9b7b3eae6fdb4a098b9db1dda85c8aaba69bec5
Sam Betts <sambetts@microsoft.com>
2026-01-29 06:38:01 +0100

    Added missing models

---
+++ src/AnalyticsEngine/Web/Models/TeamAuthStatusResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Web.AnalyticsWeb.Models
+{
+    /// <summary>
+    /// Used to list access tokens for Teams
+    /// </summary>
+    public class TeamAuthStatusResponse
+    {
+        [JsonProperty("teamId")]
+        public string TeamId { get; set; }
+
+        [JsonProperty("hasAuthToken")]
+        public bool HasAuthToken { get; set; }
+    }
+}
