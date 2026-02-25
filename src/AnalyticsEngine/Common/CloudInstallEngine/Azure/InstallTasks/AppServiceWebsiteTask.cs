b918a6cdc3f607106e77849cc85a2839d6dc1195
Sam Betts <sambetts@microsoft.com>
2026-02-25 18:03:09 +0100

    Enable basic publishing credentials for App Service
    Added logic to enable SCM and FTP publishing credentials for Azure App Service websites. This includes creating and updating publishing credentials policies to allow basic authentication, with log messages indicating the actions performed.

--- src/AnalyticsEngine/Common/CloudInstallEngine/Azure/InstallTasks/AppServiceWebsiteTask.cs
+++ src/AnalyticsEngine/Common/CloudInstallEngine/Azure/InstallTasks/AppServiceWebsiteTask.cs
@@ -67,6 +67,18 @@ namespace CloudInstallEngine.Azure.InstallTasks
                 _logger.LogInformation($"Using existing App Service '{webApp.Data.DefaultHostName}'.");
             }
 
+            // Enable basic publishing credentials for SCM and FTP
+            var publishingCredentialsPolicyData = new CsmPublishingCredentialsPoliciesEntityData()
+            {
+                Allow = true,
+            };
+
+            _logger.LogInformation($"Enabling basic publishing credentials (SCM & FTP) for '{_config.ResourceName}'...");
+            await webApp.GetScmSiteBasicPublishingCredentialsPolicy().CreateOrUpdateAsync(
+                WaitUntil.Completed, publishingCredentialsPolicyData);
+            await webApp.GetWebSiteFtpPublishingCredentialsPolicy().CreateOrUpdateAsync(
+                WaitUntil.Completed, publishingCredentialsPolicyData);
+
             return webApp;
 
         }
